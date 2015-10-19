b272476edbdac2a33883bf7ae7e98fb72c2f1afa
Knugn <david.ryman@hotmail.com>
2015-10-19 11:44:32 +0000

    improved utils

--- swiftutil.py
+++ swiftutil.py
@@ -2,6 +2,7 @@ import swiftclient.client
 
 def bucketfilelinegen(swiftconnection, bucketname, filename, chunksize = 1024*1024):
     (obj_header, obj_body) = swiftconnection.get_object(bucketname, filename, resp_chunk_size=chunksize)
+    linerem = ''
     for chunk in obj_body:
         first = True
         for s in chunk.split('\n'):
@@ -11,3 +12,9 @@ def bucketfilelinegen(swiftconnection, bucketname, filename, chunksize = 1024*10
                 yield linerem
                 linerem = ''
             linerem += s
+
+def bucketfilelinecount(swiftconnection, bucketname, filename, chunksize = 1024*1024):
+    linecount = 0
+    for line in bucketfilelinegen(swiftconnection, bucketname, filename, chunksize):
+        linecount += 1
+    return linecount
--- tweetutil.py
+++ tweetutil.py
@@ -1,11 +1,29 @@
 import re
 import json
 
+pronounlist = ['han', 'hon', 'den', 'det', 'denna', 'denne', 'hen']
 pronounregex = re.compile(r'^(han|hon|den|det|denna|denne|hen)$')
 
-def countpronouns(jsontweet, pcountdict):
+def countpronouns(jsontweet, pcountdict, ignoreretweets = True):
     tweetdict = json.loads(jsontweet)
     tweettext = tweetdict["text"]
-    pronounlist = filter(pronounregex.match, tweettext.lower().split(' '))
+    if ignoreretweets and tweettext.startswith('RT'):
+        return 0
+    pronounlist = filter(pronounregex.match, tweettext.lower().split())
     for w in pronounlist:
         pcountdict[w]+=1
+    return 1
+
+def countpronounsintweetfile(linegen, ignoreretweets = True):
+    pcountdict = {}
+    for p in pronounlist:
+        pcountdict[p] = 0
+    linecount = 0
+    tweetcount = 0
+    for line in linegen:
+        linecount += 1
+        if (linecount % 2 == 0):
+            assert(line == '')
+            continue
+        tweetcount += countpronouns(line, pcountdict, ignoreretweets)
+    return (linecount, tweetcount, pcountdict)
