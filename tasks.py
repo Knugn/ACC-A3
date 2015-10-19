b107dda3aa577d6b66986f82a0adc02907a048c8
Knugn <david.ryman@hotmail.com>
2015-10-19 16:50:09 +0000

    task definition

---
+++ tasks.py
@@ -0,0 +1,24 @@
+from celery import Celery
+import swiftutil
+import tweetutil
+
+app = Celery('tasks', backend='amqp', broker='amqp://')
+
+@app.task(ignore_result=True)
+def print_hello():
+    print 'hello there'
+
+@app.task
+def gen_prime(x):
+    multiples = []
+    results = []
+    for i in xrange(2, x+1):
+        if i not in multiples:
+            results.append(i)
+            for j in xrange(i*i, x+1, i):
+                multiples.append(j)
+    return results
+
+@app.task
+def count_pronouns(filename, bucketname="tweets"):
+    return tweetutil.countpronounsintweetfile(swiftutil.bucketfilelinegen(swiftutil.getswiftconnection(), bucketname, filename))
