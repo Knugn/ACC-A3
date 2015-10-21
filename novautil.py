d1075cd4dbe8ffbd5968b96d13f0a1e00cdfa97c
Knugn <david.ryman@hotmail.com>
2015-10-21 19:59:10 +0000

    Bug fixes and remove renamed files

--- novautil.py
+++ novautil.py
@@ -2,7 +2,7 @@ import os
 import novaclient.client
 
 def getnovaclient():
-    config = {'user':os.environ['OS_USERNAME'],
+    config = {'username':os.environ['OS_USERNAME'],
               'api_key':os.environ['OS_PASSWORD'],
               'project_id':os.environ['OS_TENANT_NAME'],
               'auth_url':os.environ['OS_AUTH_URL']
--- workerspawnutil.py
+++ workerspawnutil.py
@@ -1,25 +1,27 @@
+import os
 import novautil
 
 nc = novautil.getnovaclient()
 workerconfig = {
-    image = nc.images.find(name='Ryman Celery Worker Base').id
-    flavor = nc.flavors.find(name='m1.medium').id
-    network = nc.networks.find(label='ext-net').id
-    key_name = nc.keypairs.list().pop().name
+    'image' : nc.images.find(name='Ryman Celery Worker Base').id,
+    'flavor' : nc.flavors.find(name='m1.medium').id,
+    'network' : nc.networks.find(label='ext-net').id,
+    #key_name = nc.keypairs.list().pop().name
+    'admin_pass' : os.environ['OS_PASSWORD']
 }
-n_worker_vms = 4
+n_worker_vms = 1
 n_celery_workers_per_vm = 4
 worker_name_prefix = 'RymanA3-worker'
 
 def getworkernamegen():
-    return worker_name_prefix + str(i) for i in xrange(n_worker_vms)
+    return ((worker_name_prefix + str(i)) for i in xrange(n_worker_vms))
 
 def spawnworkers(nc, namegen, **kwargs):
     workers = []
     for name in namegen:
-        workers.add(nc.servers.create(name, **kwargs))
+        workers.append(nc.servers.create(name, **kwargs))
     return workers
 
 if __name__ == "__main__":
-    spawnworkers(nc, getworkernamegen(), workerconfig)
+    spawnworkers(nc, getworkernamegen(), **workerconfig)
 
